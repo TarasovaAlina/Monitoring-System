f0e667f85b90122ed32c4fcb091bbc7847dbc3e0
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-09-15 10:47:53 +0300

    Added exception handling within the constructor and error reporting via the `isCorrect` method

--- src/core/kernel.cpp
+++ src/core/kernel.cpp
@@ -2,17 +2,26 @@
 
 namespace core {
     Kernel::Kernel() noexcept
-    : _kernel_manager(std::make_unique<KernelManager>())
-    , _logger(std::make_unique<Logger>())
-    , _notification_service(std::make_unique<NotificationService>())
-    , _work_flag(true) {
-        _search_agents_thread = std::thread
+    : _work_flag(true) {
+        try {
+            _kernel_manager = std::make_unique<KernelManager>();
+            _logger = std::make_unique<Logger>();
+            _notification_service = std::make_unique<NotificationServer>();
+
+            _search_agents_thread = std::thread(&Kernel::_searchNewAgents, this);
+
+        } catch (const std::exception& e) {
+            _work_flag.store(false);
+        }
     }
 
     Kernel::~Kernel() noexcept {
         _work_flag.store(false);
     }
 
+    bool Kernel::isCorrect() noexcept {
+        return _work_flag.load();
+    }
 
     void Kernel::_searchNewAgents() noexcept {
         // Происходит поиск до тех пор, пока нет сигнала завершения
