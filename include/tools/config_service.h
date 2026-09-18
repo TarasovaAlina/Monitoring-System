5d53ec168872f192b461eaf15293cb475d35ef7f
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-09-18 19:54:26 +0300

    Moved the description of the ConfigService class to the "core" namespace. Corrected the formatting of the header file

--- include/tools/config_service.h
+++ include/tools/config_service.h
@@ -1,56 +1,62 @@
-#include <fstream>
-using json = nlohmann::json;
+#ifndef SYSTEM_MONITORING_CONFIG_SERVICE_H
+#define SYSTEM_MONITORING_CONFIG_SERVICE_H
+
+#include <string>
+#include <vector>
 
 /**
  * @file config_service.h
- * @brief В этом файле описан класс сервера,
+ * @brief В этом файле описан класс сервиса,
  * который будет возвращать данные из config для дальнейшей работы с агентами
  * @author Tarasova Alina
  * @date 18.09.2026
  */
 
-/**
+namespace core {
+    /**
  * @struct Threshold
  * @brief Содержит информацию о значении конкретной метрике и ператоре сравнения
  * таким как <, <=, ==, >=, >
  */
-struct Threshold {
-    std::string operation; ///< Оператор сравнения
-    double value; ///< Значение метрики
-};
+    struct Threshold {
+        std::string operation; ///< Оператор сравнения
+        double value; ///< Значение метрики
+    };
 
-/**
- * @struct MetricConfig
- * @brief Содержит информацию об отдельной метрике
- */
-struct MetricConfig {
-    std::string target; ///< Название метрики
-    Threshold threshold; ///< Структура с численным значением и оператором сравнения
-};
-
-/**
- * @struct ConfigInfo
- * @brief Содержит информацию о конкретном агенте:
- * имени агента, типе агента, интервале обновления метрик и
- * сам список метрик
- */
-
-struct ConfigInfo {
-    std::string agentName_; ///< Имя агента
-    std::string agentType_; ///< Тип агента (соответствует AgentType)
-    int updateInterval_; ///< Время обновления метрик
-    std::vector<MetricConfig> metricConfig_; ///< Список критических значений метрик
-};
-
-/**
- * @class ConfigService
- * @brief Класс по работе с config файлом
- */
-class ConfigService {
-public:
     /**
-     * @brief Обработка config файла для последующей загрузки в агентов
-     * @return вектор с информацией для каждого агента с соответствующими параметрами
+     * @struct MetricConfig
+     * @brief Содержит информацию об отдельной метрике
      */
-    std::vector<ConfigInfo> load() const override;
-};
+    struct MetricConfig {
+        std::string target; ///< Название метрики
+        Threshold threshold; ///< Структура с численным значением и оператором сравнения
+    };
+
+    /**
+     * @struct ConfigInfo
+     * @brief Содержит информацию о конкретном агенте:
+     * имени агента, типе агента, интервале обновления метрик и
+     * сам список метрик
+     */
+    struct ConfigInfo {
+        std::string agentName; ///< Имя агента
+        std::string agentType; ///< Тип агента (соответствует AgentType)
+        int updateInterval; ///< Время обновления метрик
+        std::vector<MetricConfig> metricConfig; ///< Список критических значений метрик
+    };
+
+    /**
+     * @class ConfigService
+     * @brief Класс по работе с config файлом
+     */
+    class ConfigService {
+    public:
+        /**
+         * @brief Обработка config файла для последующей загрузки в агентов
+         * @return вектор с информацией для каждого агента с соответствующими параметрами
+         */
+        std::vector<ConfigInfo> load() const;
+    };
+}
+
+#endif
