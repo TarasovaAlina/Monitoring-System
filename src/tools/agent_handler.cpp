85971b453aa72c98311902e41811437d56020f15
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-09-18 19:31:08 +0300

    Initial implementation of the AgentHandler class

---
+++ src/tools/agent_handler.cpp
@@ -0,0 +1,71 @@
+#include "tools/agent_handler.h"
+#include <dlfcn.h>
+#include <thread>
+
+namespace core {
+    AgentHandler::AgentHandler(const std::string &path, int milliseconds)
+    : _running(true)
+    , _sleeping(false)
+    , _timeout(milliseconds) {
+        _lib_agent = dlopen(path.c_str(), RTLD_LAZY);
+
+        if (_lib_agent == nullptr) {
+            throw std::runtime_error("Failed to load library: " + path);
+        }
+
+        auto updateFunc = dlsym(_lib_agent, "updateMetrics");
+        auto getMetricsFunc = dlsym(_lib_agent, "getMetrics");
+
+        if (!updateFunc || !getMetricsFunc) {
+            throw std::runtime_error("Failed to load symbols from the library: " + path);
+        }
+
+        _updateMetricsCallback = updateFunc();
+        _updateMetricsCallback = getMetricsFunc();
+    }
+
+    AgentHandler::~AgentHandler() noexcept {
+        unload();
+    }
+
+    void AgentHandler::work() noexcept {
+        while (_running.load()) {
+            // Обновление метрик только если агент находится в активном состоянии
+            if (!_sleeping.load()) {
+                _updateMetricsCallback();
+
+                _metrics = _gettingMetricsCallback();
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(_timeout));
+            }
+        }
+    }
+
+    void AgentHandler::setSleepMode(bool is_sleep) noexcept {
+        if (_sleeping.load() != is_sleep) {
+            _sleeping.store(is_sleep);
+        }
+    }
+
+    void AgentHandler::unload() noexcept {
+        if (_lib_agent != nullptr) {
+            dlclose(_lib_agent);
+
+            _lib_agent = nullptr;
+            _running.store(false);
+        }
+    }
+
+    std::string &AgentHandler::name() noexcept {
+        return _name;
+    }
+
+    std::chrono::milliseconds &AgentHandler::timeout() noexcept {
+        return _timeout;
+    }
+
+    std::vector<agent::Metric> &AgentHandler::metrics() noexcept {
+        return _metrics;
+    }
+
+}
