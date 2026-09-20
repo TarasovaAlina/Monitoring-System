c23a93a55e5897448aa7b28e9141b7516999abd2
Georgiy Kovalev <goshakovalev5@gmail.com>
2026-09-20 16:07:16 +0300

    An implementation of the CPUMetricsCollector class, taken from the `cpu/agent.cpp` file

---
+++ src/agent/cpu_metrics_collector.cpp
@@ -0,0 +1,53 @@
+#include "agent/metrics_collector.h"
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <thread>
+
+namespace agent {
+    std::vector<Metric> CPUMetricsCollector::update() noexcept {
+        auto stat1 = _readCPUStats();
+        std::this_thread::sleep_for(std::chrono::microseconds(500));
+        auto stat2 = _readCPUStats();
+
+        // Обновляем статистику
+        auto cpu = 1.0 - static_cast<double>(stat2.first - stat1.first) / (stat2.second - stat1.second);
+        auto processes = _readCountProcesses();
+
+        return {
+            Metric("cpu", cpu),
+            Metric("processes", processes)};
+    }
+
+    int CPUMetricsCollector::_readCountProcesses() noexcept {
+        // Проходимся по содержимому директории /proc и ищем файлы с целочисленным названием
+        int count = 0;
+
+        for (auto const& dir_entry : std::filesystem::directory_iterator("/proc")) {
+            // Если в строке будет символ, отличный от цифры, то вернет 0
+            count += (std::atol(dir_entry.path().filename().string().c_str()) > 0);
+        }
+
+        return count;
+    }
+
+    std::pair<unsigned long long, unsigned long long> CPUMetricsCollector::_readCPUStats() noexcept {
+        std::ifstream file("/proc/stat");
+        if (!file.is_open()) return {0, 0};
+
+        std::string line;
+        std::getline(file, line); // Считываем первую строку "cpu ..."
+        std::stringstream ss(line);
+
+        std::string label;
+        ss >> label; // Считываем метку 'cpu'
+
+        unsigned long long user, nice, system, idle, iowait, irq, softirq, steal;
+        ss >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
+
+        unsigned long long idleTime = idle + iowait; // Время задержки
+        unsigned long long totalTime = user + nice + system + idle + iowait + irq + softirq + steal;
+
+        return {idleTime, totalTime};
+    }
+}
